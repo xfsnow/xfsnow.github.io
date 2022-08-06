e9b2d7fd01309ddf32633cf4d7f376a633e4b3fe
xfsnow <xuefeng@snowpeak.org>
2022-08-06 15:43:19 +0800

    更新页面，增加英文说明

--- AzureIcon/icon_index.py
+++ AzureIcon/icon_index.py
@@ -22,9 +22,12 @@ def main():
 <title>Azure Icon SVG</title>
 </head>
 <body>
-<h1>Azure Icon 展示页</h1>
+<h1>Azure Icon Index / Azure  图标展示页</h1>
 <p>Azure 的图标可以从微软官网下载 <a href="https://docs.microsoft.com/en-us/azure/architecture/icons/"  target="_blank">https://docs.microsoft.com/en-us/azure/architecture/icons/</a>。但是这里下载的是个压缩包，解压后都是SVG文件，预览和查找都不太方便。
    我使用这个简单的 <a href="https://github.com/xfsnow/xfsnow.github.io/blob/master/AzureIcon/icon_index.py" target="_blank">Python 脚本</a>，遍历这些子目录和文件，生成一个静态 HTML 文件，就可以简单明了地展示和用服务名称查找了。</p>
+<p>The Azure icon can be downloaded for <a href="https://docs.microsoft.com/en-us/azure/architecture/icons/"  target="_blank">https://docs.microsoft.com/en-us/azure/architecture/icons/</a> from Microsoft's website.
+However the file downloaded here is a compression package which contains a large number of SVG files. It is not very convenient to preview and search so many SVG files.
+This simple Python script goes through the subdirectories and files to generate a static HTML file that include all the SVG images. We can simply preview all the icon images and search icon by service names. </p>
 <ul class="icons">
 """
     fileAll = findAllFile(basePath)
