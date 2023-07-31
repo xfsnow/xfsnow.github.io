458be6c8a13db307408b5d4320aebb8efc557d04
Feng Xue <xuefeng@snowpeak.org>
2023-07-31 19:29:20 +0800

    恢复回没有内容的状态

--- Cobra/Numbers.js
+++ Cobra/Numbers.js
@@ -2,20 +2,10 @@
 // 123456 转换成 壹拾贰万叁仟肆佰伍拾陆 
 // 7890123 转换成 柒佰捌拾玖万零壹佰贰拾叁
 // 1234567890123 转换成 壹仟贰佰叁拾肆亿伍仟陆佰柒拾捌万玖仟零壹佰贰拾叁
-const numToChinese = function(num) {
-    let str = num.toString();
-    let len = str.length;
-    let result = '';
-    let numArr = ['零','壹','贰','叁','肆','伍','陆','柒','捌','玖'];
-    let unitArr = ['','拾','佰','仟','万','拾','佰','仟','亿','拾','佰','仟','万'];
-    for(let i = 0; i < len; i++) {
-        result += numArr[parseInt(str[i])] + unitArr[len - i - 1];
-    }
-    return result;
-}
 
 /* 创建一个字典，包含北京各月的天气信息*/
 // var beijing_weather = [
+
 
 /* 创建一个字典，包含布宜诺斯艾利斯各月的天气信息*/
 
